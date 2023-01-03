fd34aea79e5a447c06b8108149968b8583baf18e
비엔나 <sure@paran.com>
2023-01-03 14:27:03 +0900

    add : header css

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -9,8 +9,8 @@ export default function Header() {
         <div className="header-container">
           <div className="header-row">
             <div className="header-logo">
-              <span>❤</span>
-              <span>Nevertheless Published Mine</span>
+              <span className="logo">❤</span>
+              <span className="text">Nevertheless Published Mine</span>
             </div>
             <nav className="header-nav">
               <ul>
@@ -29,6 +29,8 @@ export default function Header() {
               </ul>
             </nav>
           </div>
+        </div>
+        <div className="header-container">
           <div className="header-row">
             <div className="search-logo">
               <svg viewBox="0 0 780 250">
@@ -37,6 +39,41 @@ export default function Header() {
                   d="M240,250h100v-50h100V0H240V250z M340,50h50v100h-50V50z M480,0v200h100V50h50v150h50V50h50v150h50V0H480z M0,200h100V50h50v150h50V0H0V200z"
                 ></path>
               </svg>
+            </div>
+            <div className="search-bar">
+              <div className="search-input">
+                <div className="input">
+                  <span>
+                    <svg
+                      width="15px"
+                      height="15px"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 18 18"
+                      aria-hidden="true"
+                    >
+                      <g
+                        stroke="none"
+                        strokeWidth="1"
+                        fill="none"
+                        fillRule="evenodd"
+                      >
+                        <g stroke="#777777" strokeWidth="1.3">
+                          <g>
+                            <path d="M13.4044,7.0274 C13.4044,10.5494 10.5494,13.4044 7.0274,13.4044 C3.5054,13.4044 0.6504,10.5494 0.6504,7.0274 C0.6504,3.5054 3.5054,0.6504 7.0274,0.6504 C10.5494,0.6504 13.4044,3.5054 13.4044,7.0274 Z"></path>
+                            <path d="M11.4913,11.4913 L17.8683,17.8683"></path>
+                          </g>
+                        </g>
+                      </g>
+                    </svg>
+                  </span>
+                  <input placeholder="Search packages"></input>
+                </div>
+              </div>
+              <button>Search</button>
+            </div>
+            <div className="search-sign">
+              <Link className="sign-up">Sign Up</Link>
+              <Link className="sign-in">Sign In</Link>
             </div>
           </div>
         </div>
