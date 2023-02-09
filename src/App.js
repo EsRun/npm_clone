2072950f59c99dd1cfdcbe664f396909fb37a255
비엔나 <sure@paran.com>
2023-02-09 16:54:57 +0900

    add : react lazy, component split

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-import { useEffect } from "react";
+import { useEffect, Suspense, lazy } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
-import { Layout, Main, Pro, Teams, Pricing, Docu, Error } from "./pages";
+import { Layout, Main } from "./pages";
 import { Login, SignUp } from "./pages/auth";
+
+const Error = lazy(() => import("./pages/Error.jsx"));
+const Pricing = lazy(() => import("./pages/Pricing.jsx"));
+const Pro = lazy(() => import("./pages/Pro.jsx"));
+const Teams = lazy(() => import("./pages/Teams.jsx"));
+const Docu = lazy(() => import("./pages/Documentation.jsx"));
 
 function App() {
   useEffect(() => {
@@ -12,11 +18,46 @@ function App() {
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<Main />}></Route>
-          <Route path="product" element={<Pricing />}></Route>
-          <Route path="product/pro" element={<Pro />}></Route>
-          <Route path="product/teams" element={<Teams />}></Route>
-          <Route path="documentation" element={<Docu />}></Route>
-          <Route path="/error" element={<Error />}></Route>
+          <Route
+            path="product"
+            element={
+              <Suspense fallback={<div>로딩 중. . .</div>}>
+                <Pricing />
+              </Suspense>
+            }
+          ></Route>
+          <Route
+            path="product/pro"
+            element={
+              <Suspense fallback={<div>로딩 중. . .</div>}>
+                <Pro />
+              </Suspense>
+            }
+          ></Route>
+          <Route
+            path="product/teams"
+            element={
+              <Suspense fallback={<div>로딩 중. . .</div>}>
+                <Teams />
+              </Suspense>
+            }
+          ></Route>
+          <Route
+            path="documentation"
+            element={
+              <Suspense fallback={<div>로딩 중. . .</div>}>
+                <Docu />
+              </Suspense>
+            }
+          ></Route>
+          <Route
+            path="/error"
+            element={
+              <Suspense fallback={<div>로딩 중. . .</div>}>
+                <Error />
+              </Suspense>
+            }
+          ></Route>
         </Route>
         <Route path="/*" element={<Navigate to="/error" replace />} />
         <Route path="/signup" element={<SignUp />}></Route>
